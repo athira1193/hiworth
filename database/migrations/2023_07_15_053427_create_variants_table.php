95a2e0ce2f0d8c9b63381d1829bfb0e21f327271
Athira <athira1193@gmail.comgit config --global user.name athira1193>
2023-07-15 09:41:50 +0400

    Models and table done

---
+++ database/migrations/2023_07_15_053427_create_variants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVariantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->text('size')->nullable();
+            $table->text('color')->nullable();
+            $table->decimal('price', 5, 2);
+            $table->bigInteger('quantity');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variants');
+    }
+}
